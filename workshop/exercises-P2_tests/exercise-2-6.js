d343a87ed3c3de742f2e12e8d55f041c6e13da6a
ari-rosie <62956748+ari-rosie@users.noreply.github.com>
2020-06-22 14:24:01 -0400

    Exercice-2.6

--- workshop/exercises-P2_tests/exercise-2-6.js
+++ workshop/exercises-P2_tests/exercise-2-6.js
@@ -18,13 +18,31 @@
 // f(["spoof", 10, 10]); // undefined
 
 function calculator(arr) {
-  // Your code here
+  if (typeof arr[1] === "number" && typeof arr[2] === "number"){
+    switch (arr[0]) {
+      case 'add':
+        return arr[1] + arr[2];
+      case 'sub':
+        return arr[1] - arr[2];
+      case 'mult':
+        return arr[1] * arr[2];
+      default:
+        return undefined;
+    }
+  } else
+    return undefined;
 }
 
 // Step 2
 // We need 8 total test cases. The first two is provided.
 expect(calculator(['mult', 2, 4]), 8);
 expect(calculator(['add', 2, 4]), 6);
+expect(calculator(['sub', 10, 1]), 9);
+expect(calculator(['add', 'apple', 4]), undefined);
+expect(calculator(['', 2, 4]), undefined);
+expect(calculator(['add', 2, '']), undefined);
+expect(calculator(['mult', 2, 4, 9]), 8);
+expect(calculator(['sub', 12, 5], 'mult'), 7);
 
 /**
  * -------------------------------------------------------------------
