c666928e2bab48382a7ac8fb0fcc177e238343fd
ari-rosie <62956748+ari-rosie@users.noreply.github.com>
2020-06-22 13:54:51 -0400

    Exercice-2.4

--- workshop/exercises-P2_tests/exercise-2-4.js
+++ workshop/exercises-P2_tests/exercise-2-4.js
@@ -10,12 +10,22 @@
 // getLetterAtIndex("abc", 0);   // a
 
 function getLetterAtIndex(str, index) {
-  // Your code here
+  return (typeof str[index] === 'string' && checkIfChar(str[index])) ? str[index] : undefined;
+}
+
+function checkIfChar(c) {
+  return (c >= "a" && c <= "z" || c >= "A" && c <= "Z") ? true : false;
 }
 
 // Add 6 more test cases
 expect(getLetterAtIndex('hello', 4), 'o');
 expect(getLetterAtIndex('goodbye', 0), 'g');
+expect(getLetterAtIndex('rodeo', 6), undefined);
+expect(getLetterAtIndex('', 0), undefined);
+expect(getLetterAtIndex(1234, 2), undefined);
+expect(getLetterAtIndex('rodeo', -1), undefined);
+expect(getLetterAtIndex('my little pony', 9), undefined);
+expect(getLetterAtIndex("cowboy's hat", 6), undefined);
 
 /**
  * -------------------------------------------------------------------
