67ff798b342724e167880e19fa246168354bf4b5
ari-rosie <62956748+ari-rosie@users.noreply.github.com>
2020-06-22 20:44:20 -0400

    Exercice-3.1

--- workshop/exercises-P3_problems/exercise-3-1/app.js
+++ workshop/exercises-P3_problems/exercise-3-1/app.js
@@ -6,3 +6,32 @@
 // Guidelines
 // - write only one event listener
 // - use a switch statement
+
+//DOM variables
+const buttons = document.querySelectorAll('button');
+
+
+const buttonEffect = btnId => {
+    let button = document.querySelector(`#${btnId}`);
+    
+    switch (btnId) {
+        case 'btn-1':
+            button.style.opacity = '0%';
+            break;
+        case 'btn-2':
+            button.style.backgroundColor = 'red';
+            break;
+        case 'btn-3':
+            button.style.backgroundColor = 'lightblue';
+            break;
+        case 'btn-4':
+            button.classList.toggle('jitters');
+            break;               
+    }
+}
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', function() {
+        buttonEffect(this.getAttribute('id'));
+    });
+});
